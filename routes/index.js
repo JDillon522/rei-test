29190050dd79657c0db95e752f75d4d53f7498f5
Joseph <joseph.dillon.522@gmail.com>
2014-11-15 17:25:05 -0500

    Tests 1 - 4 done

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Barkley REI Frontend Test' });
+});
+
+router.get('/test1', function(req, res) {
+  res.render('test1', { title: 'Test 1 - Use the hCalendar Microformat' });
+});
+
+router.get('/test2', function(req, res) {
+  res.render('test2', { title: 'Test 2 - Write semantic markup' });
+});
+
+router.get('/test3', function(req, res) {
+  res.render('test3', { title: 'Test #3 - Basic jQuery' });
+});
+
+router.get('/test4', function(req, res) {
+  res.render('test4', { title: 'Test #4 - Basic JavaScript' });
+});
+
+router.get('/test5', function(req, res) {
+  res.render('test5', { title: 'Test #5 - Implement Flexslider' });
+});
+
+router.get('/test6', function(req, res) {
+  res.render('test6', { title: 'Test #6 - Responsive' });
+});
+
+router.get('/test7', function(req, res) {
+  res.render('test7', { title: 'Test #7 - Make a jQuery Plugin' });
+});
+
+module.exports = router;
